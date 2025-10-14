efa53e858dd96a9514159e703d23c7cdec53d923
Jmauratille <jeanmauratille@Jeans-MBP.attlocal.net>
2025-10-14 16:32:41 -0500

    completed functions: is_new_order_single, is_filled_limit_exec

--- Pset1/fix_to_csv.py
+++ Pset1/fix_to_csv.py
@@ -33,8 +33,21 @@ def parse_fix_line(line):
 
     return prefix_ts, fields
 
-sample = "20250910-00:02:34.713753 : 8=FIX.4.29=33035=834=196249=ID152=20250910-04:02:34.71356=ID21=ID66=32.9811=ID314=1017=ID520=030=ID731=32.9832=1037=ID438=1039=240=244=32.9854=255=SMCX59=060=20250910-04:02:34.71376=ID8150=2151=010=235"
-ts, f = parse_fix_line(sample)
-print("prefix_ts:", ts)
-print("tag 35:", f.get("35"))
-print("all fields:", f)
+def is_new_order_single(fields):
+    """Return True if MsgType (35) is NewOrderSingle (D)."""
+    return fields.get("35") == "D"
+
+def is_filled_limit_exec(fields):
+    """
+    Return True only for filled:
+      35=8  (ExecutionReport)
+      150=2 (ExecType=FILL)
+      39=2  (OrdStatus=FILLED)
+      40=2  (OrdType=LIMIT)
+    """
+    return (
+        fields.get("35") == "8"
+        and fields.get("150") == "2"
+        and fields.get("39") == "2"
+        and fields.get("40") == "2"
+    )
